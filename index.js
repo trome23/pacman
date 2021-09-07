23f6892a0f58911b7fedfc19065e393f2e1ac060
Tyson Romero <romennoodles23@yahoo.com>
2021-09-06 23:30:57 -0600

    layout is made using dynamic JS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const width = 28
 const grid = document.querySelector(".grid")
 const scoreDisplay = document.querySelector("#score")
-
+let squares = []
 
 //28 * 28 = 784
   // 0 - pac-dots
@@ -39,3 +39,24 @@ const layout = [
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
 ]
+
+//create board
+const createBoard = () => {
+    for (let i = 0; i < layout.length; i++) {
+        const square = document.createElement('div')
+
+        grid.appendChild(square)
+        
+        squares.push(square)
+        
+        if (layout[i] === 0) {
+            squares[i].classList.add('pac-dot')
+        } else if  (layout[i] === 1) {
+            squares[i].classList.add('wall')
+        } else if  (layout[i] === 3) {
+            squares[i].classList.add('power-pellet')
+        } 
+    }
+}
+createBoard()
+
