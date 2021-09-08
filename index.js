78d42f577fcd2b5fa3ae6890030ccf58ac884826
Tyson Romero <romennoodles23@yahoo.com>
2021-09-08 11:53:45 -0600

    game works now just needs personalized styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const width = 28
-const grid = document.querySelector(".grid")
-const scoreDisplay = document.querySelector("#score")
-let squares = []
+const width = 28  //<== width of each square
+const grid = document.querySelector(".grid")  //<==grabbing our grid from DOM
+const scoreDisplay = document.querySelector("#score")  //<==grabbing our 'scoreboard' from DOM
+let squares = []  //<==empty array for our grid squares
 let score = 0
 
 
@@ -42,15 +42,16 @@ const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
 ]
 
-//create board
+//creating pacman grid layout
 const createBoard = () => {
     for (let i = 0; i < layout.length; i++) {
-        const square = document.createElement('div')
+        const square = document.createElement('div')  //<== running for loop to create divs for each square dynamically
 
-        grid.appendChild(square)
+        grid.appendChild(square)   //<== adding dynamically made squares to grid on DOM
         
-        squares.push(square)
+        squares.push(square)   //<== pushing square to empty squares array
         
+        //logic for adding squares based on number aligning with their class in CSS
         if (layout[i] === 0) {
             squares[i].classList.add('pac-dot')
         } else if  (layout[i] === 1) {
@@ -62,49 +63,66 @@ const createBoard = () => {
         } 
     }
 }
-createBoard()
+createBoard()  //<== calling function to create pacman grid
 
 //starting position of pacman
 let pacmanCurrentIndex = 490
 squares[pacmanCurrentIndex].classList.add('pacman')
 
+// function that moves pacman using arrow keys
 const control = (e) => {
-    squares[pacmanCurrentIndex].classList.remove('pacman')
+    squares[pacmanCurrentIndex].classList.remove('pacman')  //<== removing pacman class so you can see him move wherever you click
     switch (e.key) {
         case 'ArrowDown':
-            if (!squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+            if (
+                !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
-                pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+                pacmanCurrentIndex + width < width * width
+                ) 
+                pacmanCurrentIndex += width
             break
         case 'ArrowUp':
-            if (!squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+            if (
+                !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
-                pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+                pacmanCurrentIndex - width >= 0
+                ) 
+                pacmanCurrentIndex -= width
             break
         case 'ArrowRight':
-            if (!squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+            if (
+                !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
-                pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+                pacmanCurrentIndex % width < width - 1
+                ) 
+                pacmanCurrentIndex += 1
                 if(pacmanCurrentIndex === 391) {
                     pacmanCurrentIndex = 364
                 }     
             break
         case 'ArrowLeft':
-            if (!squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+            if (
+                !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
                 !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
-                pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+                pacmanCurrentIndex % width !== 0
+                ) 
+                pacmanCurrentIndex -= 1
                 if(pacmanCurrentIndex === 364) {
                     pacmanCurrentIndex = 391
                 }    
             break
     }
-    squares[pacmanCurrentIndex].classList.add('pacman')
+    squares[pacmanCurrentIndex].classList.add('pacman')  //<== adding class so you can see pacman move to new position
     eatPacDot()
     eatPowerPellet()
+    checkForWin()
+    gameOverCheck()
 }
-document.addEventListener('keyup', control)
+document.addEventListener('keydown', control)  //<== eventlistener for arrow keys, and calling control function when clicked
 
+//logic for what happens when pacman eats dots
 const eatPacDot = () => {
+    //get pacman current location and see if he is over a 'pacdot' div, and if so, removing that class & adding to score
     if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
         squares[pacmanCurrentIndex].classList.remove('pac-dot')
         score++
@@ -112,7 +130,9 @@ const eatPacDot = () => {
     }
 }
 
+//logic for what happens when pacman eats power-pellet
 const eatPowerPellet = () => {
+    //check to see if pacman is eating power-pellet and if so, removing that class, adding to score and making ghosts 'scared'
     if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
         squares[pacmanCurrentIndex].classList.remove('power-pellet')
         score += 10
@@ -122,10 +142,12 @@ const eatPowerPellet = () => {
     }
 }
 
+//logic for setting ghosts back to normal after 10 seconds of being 'scared'
 let unScareGhost = () => {
     ghosts.forEach(ghost => ghost.isScared = false)
 }
 
+//creating a class constructor so we can ghost object
 class Ghost {
     constructor(className, startIndex, speed) {
         this.className = className
@@ -137,6 +159,7 @@ class Ghost {
     }
 }
 
+//declaring our ghosts names, startingIndex and speed
 const ghosts = [
     new Ghost("Todd", 348, 250),
     new Ghost("Jerry", 376, 300),
@@ -144,18 +167,21 @@ const ghosts = [
     new Ghost("Reggie", 379, 500)
 ]
 
+//forEach function to add specific ghost's assigned classname, and, additional class to grab all ghosts
 ghosts.forEach(ghost => {
     squares[ghost.startIndex].classList.add(ghost.className)
     squares[ghost.startIndex].classList.add('ghost')
 })
 
+//logic for moving ghosts randomly
 let moveGhost = (ghost) => {
     const directions = [-1, +1, -width, +width]
     let direction = directions[Math.floor(Math.random() * directions.length)]
 
+    //using setInterval to set time for ghosts to move around until eaten or game over
     ghost.timerId = setInterval(() => {
         if (
-            !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+            !squares[ghost.currentIndex + direction].classList.contains('wall') &&  //<== stop ghosts from hitting wall and/or eachother
             !squares[ghost.currentIndex + direction].classList.contains('ghost')
             ) {
             squares[ghost.currentIndex].classList.remove(ghost.className)
@@ -165,11 +191,16 @@ let moveGhost = (ghost) => {
             squares[ghost.currentIndex].classList.add('ghost')
         } else direction = directions[Math.floor(Math.random() * directions.length)]
 
+        //adding class to scared-ghost
         if (ghost.isScared) {
             squares[ghost.currentIndex].classList.add('scared-ghost')
         }
 
-        if(ghost.isScared && squares[ghost.currentIndex].classList.contains('pacman')) {
+        //if ghost is scared and pacman hits them, then remove classes from ghosts, add 50 to score and send ghost to startingIndex in ghost lair
+        if(
+            ghost.isScared && 
+            squares[ghost.currentIndex].classList.contains('pacman')
+        ) {
             squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
             ghost.currentIndex = ghost.startIndex
             score += 50
@@ -177,17 +208,32 @@ let moveGhost = (ghost) => {
             squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
         }
         gameOverCheck()
+        
     }, ghost.speed);
     
 }
 
+//<== forEach function to get each ghost to move
 ghosts.forEach(ghost => moveGhost(ghost))
 
-let gameOverCheck = () => {
-    if (squares[pacmanCurrentIndex].classList.contains('ghost') && 
-    !squares[pacmanCurrentIndex].classList.contains('scared-ghost')) {
-        ghosts.forEach(ghost=> clearInterval(ghost.timerId))
-        document.removeEventListener('keyup', control)
+// end game if ghost gets pacman
+const gameOverCheck = () => {
+
+    if (
+        squares[pacmanCurrentIndex].classList.contains('ghost') && 
+        !squares[pacmanCurrentIndex].classList.contains('scared-ghost')
+    ) {
+        ghosts.forEach(ghost=> clearInterval(ghost.timerId))  //<== stop ghosts from moving
+        document.removeEventListener('keydown', control) //<== stop controls from being active
         scoreDisplay.innerHTML = 'GAME OVER'
+    }
+}
+
+//check to see if all dots are eaten, and declare winner if so!
+const checkForWin = () => {
+    if (score === 274) {
+        ghosts.forEach(ghost => clearInterval(ghost.timerId))
+        document.removeEventListener('keydown', control)
+        scoreDisplay.innerHTML = 'Winner winner chicken dinner!'
     }
 }
